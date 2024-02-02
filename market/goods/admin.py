b06a4e81072d32229cb5f65969152bfce0e86005
Prohor <DKoval777@yandex.ru>
2024-02-02 19:55:10 +0300

    asd

--- market/goods/admin.py
+++ market/goods/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from goods.models import *
 
 # Register your models here.
+
+# admin.site.register(Categories)
+# admin.site.register(Products)
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):  # автоматическое заполнение поля 'slug' по названию поля 'name'
+    prepopulated_fields = {
+        'slug': ('name',)
+    }
+
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):  # автоматическое заполнение поля 'slug' по названию поля 'name'
+    prepopulated_fields = {
+        'slug': ('name',)
+    }
