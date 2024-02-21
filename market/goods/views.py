adab5517ac80df139db6e7c7591b4f5f12c0acf3
Prohor <DKoval777@yandex.ru>
2024-02-21 20:59:38 +0300

    oiugiyf

--- market/goods/views.py
+++ market/goods/views.py
@@ -5,15 +5,30 @@ from goods.models import Products
 # Create your views here.
 
 
-def catalog(request):
+def catalog(request, category_slug):
+    """Выводим товары по категориям, если в категории нет товаров,
+    то выводится "Ничего нет" """
 
-    goods = Products.objects.all()
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+
+        if not goods.exists():
+            return render(request, 'goods/index.html', {"content": "Ничего нет"})
+
     context = {
-        'title': 'Home-Каталог',
+        'title': 'Каталог',
         'goods': goods,
     }
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    """ Достаем из бд id каждого товара"""
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        'product': product
+    }
+    return render(request, 'goods/product.html', context=context)
