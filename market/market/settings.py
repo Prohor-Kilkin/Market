fc10a7735e66ad60fc7f785bf0b657d196a6fd51
Prohor <DKoval777@yandex.ru>
2024-03-06 20:51:32 +0300

    users

--- market/market/settings.py
+++ market/market/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users'
 
 ]
 
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
